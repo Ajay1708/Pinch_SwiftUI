05af8bfe07a35b2ffbfea3e86659bb359e35ef8e
VenkataParas <venkata@parastechnologies.com>
2022-08-07 12:41:24 +0530

    Initial Commit

---
+++ Pinch_SwiftUI/Pinch_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pinch_SwiftUIApp.swift
+//  Pinch_SwiftUI
+//
+//  Created by Apple on 07/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Pinch_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
